42d764039f6d71b9930625d261243e207ba09f42
Jaime Diniz <jaimediniz@gmail.com>
2020-12-25 22:14:48 +0000

    Hide test button

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { SweetAlertService } from 'src/app/services/sweetAlert.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public testButton = false;
   constructor(private api: API) {}
 
   ngOnInit(): void {}
