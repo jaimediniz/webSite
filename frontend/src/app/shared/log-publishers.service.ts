0fe44b178fe8025ae48de6de5e248eb73636ff5f
Jaime Diniz <jaimediniz@gmail.com>
2020-12-26 15:25:01 +0000

    Remove localstorage log for production

--- frontend/src/app/shared/log-publishers.service.ts
+++ frontend/src/app/shared/log-publishers.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 import { LogPublisher, LogConsole, LogLocalStorage } from './log-publisher';
 
@@ -16,6 +17,10 @@ export class LogPublishersService {
   buildPublishers(): void {
     // Create instance of LogConsole Class
     this.publishers.push(new LogConsole());
+
+    if (environment.production) {
+      return;
+    }
 
     // Create instance of `LogLocalStorage` Class
     this.publishers.push(new LogLocalStorage());
