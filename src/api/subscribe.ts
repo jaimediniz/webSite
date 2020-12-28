705202c72281927d21b87d54d0e23e65668d2d7f
Jaime Diniz <jaimediniz@gmail.com>
2020-12-28 17:08:19 +0000

    Add gulp

--- src/api/subscribe.ts
+++ src/api/subscribe.ts
@@ -2,41 +2,24 @@ import fetch from 'node-fetch';
 import Status from 'http-status-codes';
 import { HttpRequest } from '@angular/common/http';
 
-interface QuerySubscription {
-  url: string;
-  body: {
-    route: string;
-    endPoint: string;
-    publicKey: string;
-    name: string;
-    endpointURL: string;
-    keyP256dh: string;
-    keyAuth: string;
-    paused: string;
-    topics: string;
-  };
-}
-
 export default async (request: HttpRequest<any>, response: any) => {
   if (request.method !== 'POST') {
     return response.status(Status.BAD_REQUEST).send('');
   }
 
-  const query: QuerySubscription = JSON.parse(request?.body);
+  if (!request.body) {
+    return response.status(Status.BAD_REQUEST).send('');
+  }
 
-  const resp = await fetch(query.url, {
+  const resp = await fetch('%baseUrl%', {
     method: 'post',
-    body: JSON.stringify(query?.body ?? 'null'),
-    headers: { 'Content-Type': 'application/json' }
+    body: request.body
   });
 
   console.log(resp);
 
   response.send({
     statusCode: 201,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: '{"code":201,"error":false,"message":"Success!"}'
+    body: '{"code":201,"error":false,"message":"???!"}'
   });
 };
