0a7ee2e35dd0ba4e0135cc5e4081bda9819f7b23
Jaime Diniz <jaimediniz@gmail.com>
2021-01-09 13:37:01 +0100

    Add prettierignore

--- api/database.js
+++ api/database.js
@@ -1,17 +1,17 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const http_status_codes_1 = require('http-status-codes');
-const dbConnection_1 = require('./dbConnection');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_status_codes_1 = require("http-status-codes");
+const dbConnection_1 = require("./dbConnection");
 exports.default = async (request, response) => {
-  if (request.method !== 'GET') {
-    return response.status(http_status_codes_1.default.BAD_REQUEST).send('');
-  }
-  const db = await dbConnection_1.connectToDatabase();
-  const listings = await db
-    .collection('Subscriptions')
-    .find({})
-    .sort({ metacritic: -1 })
-    .limit(20)
-    .toArray();
-  return response.status(http_status_codes_1.default.ACCEPTED).json(listings);
+    if (request.method !== 'GET') {
+        return response.status(http_status_codes_1.default.BAD_REQUEST).send('');
+    }
+    const db = await dbConnection_1.connectToDatabase();
+    const listings = await db
+        .collection('Subscriptions')
+        .find({})
+        .sort({ metacritic: -1 })
+        .limit(20)
+        .toArray();
+    return response.status(http_status_codes_1.default.ACCEPTED).json(listings);
 };
