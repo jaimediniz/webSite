084f277a5b11c6fb285f2ff73634d7ccdf823a33
Jaime Diniz <jaimediniz@gmail.com>
2021-02-13 10:49:39 +0100

    Add export ics function

--- frontend/src/app/components/pages/schedules/schedules.component.ts
+++ frontend/src/app/components/pages/schedules/schedules.component.ts
@@ -1,4 +1,7 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import * as ics from 'ics';
+
 import { APIService } from 'src/app/services/backend.service';
 import { Event } from '../../../interfaces/database';
 
@@ -9,13 +12,66 @@ import { Event } from '../../../interfaces/database';
 })
 export class SchedulesComponent implements OnInit {
   public events: Array<Event>;
+  public hideExportButton = true;
 
   constructor(private api: APIService) {
     this.api.getEvents().then((response: any) => {
       console.log(response);
       this.events = response.message;
+      this.hideExportButton = false;
     });
   }
 
   ngOnInit(): void {}
+
+  exportSchedule() {
+    let exportEvents: any = [];
+    let processedItems = 0;
+    this.events.forEach((event) => {
+      const newEvent = {
+        calName: 'NICE Events',
+        title: event.name,
+        description: event.description,
+        status: event.status,
+        organizer: { name: event.author },
+        start: new Date(event.start)
+          .toISOString()
+          .split('.')[0]
+          .split(/(?:-| |T|:)+/)
+          .map((x) => +x),
+        end: new Date(event.end)
+          .toISOString()
+          .split('.')[0]
+          .split(/(?:-| |T|:)+/)
+          .map((x) => +x),
+        location: event.location,
+        url: event.url
+      };
+      exportEvents.push(newEvent);
+      processedItems += 1;
+
+      if (processedItems === this.events.length) {
+        const { error, value } = ics.createEvents(exportEvents);
+
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        if (!value) {
+          return;
+        }
+
+        const blob = new Blob([value], { type: 'application/octet-stream' });
+        const url = window.URL.createObjectURL(blob);
+        const date = new Date().toISOString().split('T')[0].split('-');
+
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = `NICE_EVENTS_${date[2]}_${date[1]}_${date[0]}.ics`;
+        a.click();
+      }
+    });
+  }
 }
