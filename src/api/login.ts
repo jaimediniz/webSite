365bc6a079d5ab880d8bf42f9cdd56a9fa5b986b
Jaime Diniz <jaimediniz@gmail.com>
2021-02-17 09:26:55 +0100

    feat: update login logic

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,46 +2,99 @@ import { Request, Response } from 'express';
 import Status from 'http-status-codes';
 import * as bcrypt from 'bcrypt';
 
-import { connectToDatabase, getBody } from './dbConnection';
+import { getBody, getAll } from './dbConnection';
+import { APILoginResponse } from 'src/interfaces/backend';
+import { Users } from 'src/interfaces/database';
 
-interface UserEntry {
-  _id: string;
-  username: string;
-  password: string;
-  role: string;
-}
+export const isValidKey = (key: string) =>
+  // TODO: Check if cookie is valid
+  true;
 
-export default async (request: Request, response: Response) => {
-  let body;
-  try {
-    body = await getBody(request.method, request.body);
-  } catch (err) {
-    return response
-      .status(Status.BAD_REQUEST)
-      .send({ error: true, message: err.message });
+const get = async (): Promise<{
+  code: number;
+  error: boolean;
+  message: string;
+  data: any;
+}> => {
+  if (!isValidKey('')) {
+    return {
+      code: Status.BAD_REQUEST,
+      error: true,
+      message: 'The cookie is expired!',
+      data: { role: 'user', key: '' }
+    };
   }
-  console.log(body);
+  return { code: Status.ACCEPTED, error: false, message: 'Success', data: {} };
+};
 
-  const db = await connectToDatabase();
-  /* cSpell:disable */
-  const users: Array<UserEntry> = (await db
-    .collection('Users')
-    .find({ username: body.username })
-    .toArray()) as any;
-  /* cSpell:enable */
+const post = async (
+  request: Request
+): Promise<{
+  code: number;
+  error: boolean;
+  message: string;
+  data: { role: string; key: string };
+}> => {
+  const body = await getBody(request.method, request.body);
+  const users: Array<Users> = (
+    await getAll('Users', {
+      username: body.username
+    })
+  ).data;
 
   if (
-    users.length !== 1 ||
+    users.length < 1 ||
     !(await bcrypt.compare(body.password, users[0].password))
   ) {
-    return response.status(Status.BAD_REQUEST).json({
+    return {
+      code: Status.BAD_REQUEST,
       error: true,
-      message: 'You have entered an invalid username or password!'
-    });
+      message: 'You have entered an invalid username or password!',
+      data: { role: 'user', key: '' }
+    };
   }
-  console.log(users[0]);
 
-  return response
-    .status(Status.ACCEPTED)
-    .json({ username: users[0].username, role: users[0].role });
+  // TODO: create cookie
+
+  return {
+    code: Status.ACCEPTED,
+    error: false,
+    message: 'Success',
+    data: { role: users[0].role, key: '123' }
+  };
+};
+
+export default async (request: Request, response: Response) => {
+  let json: APILoginResponse;
+  try {
+    let result;
+    if (request.method === 'GET') {
+      result = await get();
+    } else if (request.method === 'POST') {
+      result = await post(request);
+    } else {
+      result = {
+        code: Status.BAD_REQUEST,
+        error: true,
+        message: 'Bad Request',
+        data: { role: '', key: '' }
+      };
+    }
+
+    json = {
+      code: result.code,
+      error: result.error,
+      message: result.message,
+      data: result.data
+    };
+    return response.status(result.code).json(json);
+  } catch (err) {
+    json = {
+      code: Status.INTERNAL_SERVER_ERROR,
+      error: true,
+      message: err.message,
+      data: { role: '', key: '' }
+    };
+    return response.status(Status.INTERNAL_SERVER_ERROR).json(json);
+  }
 };
