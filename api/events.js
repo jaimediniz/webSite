3e1466feae8b1afce4d102bc09b7c07e686363cc
Jaime Diniz <jaimediniz@gmail.com>
2021-02-19 20:42:34 +0100

    fix: remove access to unguarded route

--- api/events.js
+++ api/events.js
@@ -5,20 +5,9 @@ const get = async (request, response) => {
     const json = await dbConnection_1.getAll('Events');
     response.status(json.code).json(json);
 };
-const post = async (request, response) => {
-    const body = await dbConnection_1.getBody(request.body);
-    if (!body) {
-        return dbConnection_1.badRequest(response);
-    }
-    const json = await dbConnection_1.insertOne('Events', body);
-    response.status(json.code).json(json);
-};
 exports.default = async (request, response) => {
     if (request.method === 'GET') {
         return await get(request, response);
-    }
-    if (request.method === 'POST') {
-        return await post(request, response);
     }
     return dbConnection_1.badRequest(response);
 };
