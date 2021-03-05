10e1dd3873af6aafd7ee775a8b888980fc5ace5e
Jaime Diniz <jaimediniz@gmail.com>
2021-03-05 17:40:31 +0100

    feat: add card style to register component

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  DomSanitizer,
-  SafeResourceUrl,
-  SafeUrl
-} from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { APIService } from 'src/app/services/backend.service';
 import { External } from 'src/interfaces/database';
+
+import { ResizedEvent } from 'angular-resize-event';
+import { WindowService } from 'src/app/services/window.service';
 
 @Component({
   selector: 'app-register',
@@ -15,10 +14,14 @@ import { External } from 'src/interfaces/database';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit, OnDestroy {
+  width: any;
+  height: any;
+
   public currentUrl: SafeResourceUrl;
   public showInfo = '';
 
   private routeSubscription: Subscription;
+
   constructor(
     private route: ActivatedRoute,
     private api: APIService,
@@ -34,6 +37,11 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {}
 
+  onResized(event: ResizedEvent) {
+    this.width = event.newWidth;
+    this.height = event.newHeight;
+  }
+
   updateState() {
     const form = this.showInfo.charAt(0).toUpperCase() + this.showInfo.slice(1);
     this.api
@@ -46,9 +54,12 @@ export class RegisterComponent implements OnInit, OnDestroy {
   }
 
   updateSrc(url: string) {
-    console.log(this.currentUrl.toString());
+    console.log(this.currentUrl);
     console.log(url);
-    if (this.currentUrl === url) {
+    if (
+      (this.currentUrl as any)?.changingThisBreaksApplicationSecurity ??
+      '' === url
+    ) {
       console.log('OK');
       return;
     }
